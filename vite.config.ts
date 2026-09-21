a41bacd74ccd0ddfb2e81a5cc3b353ed8bb4e49a
Nithish M <maninithish56@gmail.com>
2026-09-21 07:12:13 +0000

    changed vite, index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/Seva-Bot/",
+
   plugins: [
     react(),
     tailwindcss(),
